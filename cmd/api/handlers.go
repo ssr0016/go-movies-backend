98af0acdaece107b1623e3a8347d6f1107449b4a
ssr0016 <samsonrecluta0016@gmail.com>
2025-08-21 21:36:41 +0800

    returning a list of movies as JSON

--- cmd/api/handlers.go
+++ cmd/api/handlers.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"backend/internal/models"
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"time"
 )
 
 func (app *application) Home(w http.ResponseWriter, r *http.Request) {
@@ -18,6 +20,49 @@ func (app *application) Home(w http.ResponseWriter, r *http.Request) {
 	}
 
 	out, err := json.Marshal(payload)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(out)
+}
+
+func (app *application) AllMovies(w http.ResponseWriter, r *http.Request) {
+	var movies []models.Movie
+
+	rd, _ := time.Parse("2006-01-02", "1986-03-07")
+
+	highlander := models.Movie{
+		ID:          1,
+		Title:       "Highlander",
+		ReleaseDate: rd,
+		MPAARating:  "R",
+		RunTime:     116,
+		Description: "In the year 1536, a Scottish Highlander named Connor MacLeod is mortally wounded in battle, but mysteriously recovers.",
+		CreatedAt:   time.Now(),
+		UpdateAt:    time.Now(),
+	}
+
+	movies = append(movies, highlander)
+
+	rd, _ = time.Parse("2006-01-02", "1981-06-012")
+
+	rotla := models.Movie{
+		ID:          2,
+		Title:       "Raiders of the Lost Ark",
+		ReleaseDate: rd,
+		MPAARating:  "PG13",
+		RunTime:     115,
+		Description: "Another classic adventure film featuring Indiana Jones.",
+		CreatedAt:   time.Now(),
+		UpdateAt:    time.Now(),
+	}
+
+	movies = append(movies, rotla)
+
+	out, err := json.Marshal(movies)
 	if err != nil {
 		fmt.Println(err)
 	}
---
+++ internal/models/movie.go
@@ -0,0 +1,15 @@
+package models
+
+import "time"
+
+type Movie struct {
+	ID          int       `json:"id"`
+	Title       string    `json:"title"`
+	ReleaseDate time.Time `json:"release_date"`
+	RunTime     int       `json:"run_time"`
+	MPAARating  string    `json:"mpaa_rating"`
+	Description string    `json:"description"`
+	Image       string    `json:"image"`
+	CreatedAt   time.Time `json:"-"`
+	UpdateAt    time.Time `json:"-"`
+}
